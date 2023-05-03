66da252c88b4d01d4460e4111056c3883b91aecc
ThisLightMan <35874605+ThisLightMan@users.noreply.github.com>
2023-05-03 11:55:03 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,80 @@
+/**
+ * @author ThisLightMan <light@corebot.dev>
+ */
+
+const Discord = require('discord.js')
+const { Client, Partials, GatewayIntentBits } = Discord
+
+const log = require('./utils/log')
+const debug = require('debug')('startup')
+
+debug('Loading better-sqlite3')
+
+const db = require('better-sqlite3')('main.db')
+db.prepare('CREATE TABLE IF NOT EXISTS message_stats (serverId, userId, timestamp)').run()
+
+debug('Loaded better-sqlite3')
+
+const EventHandler = require('./handlers/EventHandler')
+const CommandHandler = require('./handlers/CommandHandler')
+
+const client = new Client({
+    autoReconnect: true,
+    partials: [Partials.Channel],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution
+    ]
+})
+
+debug('Loading console-stamp')
+
+// Console Stamp Setup
+require('console-stamp')(console)
+
+debug('Logging into Discord')
+const config = require('./config')
+
+client.login(config.Token)
+    .then(() => {
+        module.exports.client = client
+
+        debug('Logged into Discord')
+        log(`Logged into Discord (${client.user.tag})`, 'info')
+
+        debug('Running EventHandler#register')
+        EventHandler.register()
+        debug('Finished running EventHandler#register')
+
+        debug('Running CommandHandler#register')
+        CommandHandler.register()
+        debug('Finished running CommandHandler#register')
+    })
+    .catch((err) => {
+        if (err.toString().includes('TokenInvalid')) {
+            debug('Failed to log into Discord: %O', err)
+            log('Failed to log into Discord. Make sure your token is valid.', 'error')
+
+            process.exit(1)
+        } else {
+            log(err, 'error')
+            throw err
+        }
+    })
